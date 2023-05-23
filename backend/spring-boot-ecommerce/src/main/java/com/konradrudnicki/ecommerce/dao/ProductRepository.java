f74c2f65ca998d47dc3701fe5eecc3108fd6d6b5
konradrudnicki <konradrudnicki0@gmail.com>
2023-05-23 10:14:08 +0200

    Release 1.0

--- backend/spring-boot-ecommerce/src/main/java/com/konradrudnicki/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/konradrudnicki/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.konradrudnicki.ecommerce.dao;
 
 import com.konradrudnicki.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
