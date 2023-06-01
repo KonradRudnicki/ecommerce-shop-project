d64e80e0686c65b212a9c70adeb4caf17cd66e36
konradrudnicki <konradrudnicki0@gmail.com>
2023-06-01 17:22:16 +0200

    Release 2.0 - connecting Country and State to frontend

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {FormService} from "../../services/form.service";
 import * as dns from "dns";
+import {Country} from "../../common/country";
+import {State} from "../../common/state";
 
 @Component({
   selector: 'app-checkout',
@@ -17,6 +19,12 @@ export class CheckoutComponent implements OnInit{
 
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
 
   constructor(private formBuilder: FormBuilder,
               private formService: FormService) {}
@@ -73,21 +81,37 @@ export class CheckoutComponent implements OnInit{
         this.creditCardYears = data;
       }
     )
+
+    //populate countries
+    this.formService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries" + JSON.stringify(data));
+        this.countries = data;
+      }
+    )
   }
 
   copyShippingAddressToBillingAddress(event: Event) {
     if((event.target as HTMLInputElement).checked) {
       this.checkoutFormGroup.controls['billingAddress']
         .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+
+      this.billingAddressStates = this.shippingAddressStates;
+
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      this.billingAddressStates = [];
     }
   }
 
   onSubmit() {
     console.log("Handling the submit button")
     console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log("The email is " + this.checkoutFormGroup.get('customer')?.value.email)
 
+    console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name)
+    console.log("The shipping address state is " + this.checkoutFormGroup.get('shippingAddress')?.value.state.name)
 
   }
 
@@ -112,5 +136,30 @@ export class CheckoutComponent implements OnInit{
         this.creditCardMonths = data;
       }
     )
+  }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName)
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.formService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName == 'shippingAddress'){
+          this.shippingAddressStates = data
+        }else {
+          this.billingAddressStates = data
+        }
+
+        formGroup?.get('state')?.setValue(data[0])
+      }
+    );
+
   }
 }
